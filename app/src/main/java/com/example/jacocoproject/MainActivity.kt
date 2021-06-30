1dd2aa44f3d44ae30b51bb84d28c163761bd670b
sergiob <sergiob@ciandt.com>
2021-06-30 19:32:10 -0300

    Teste CI

--- app/src/main/java/com/example/jacocoproject/MainActivity.kt
+++ app/src/main/java/com/example/jacocoproject/MainActivity.kt
@@ -7,5 +7,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Check that CI holds lint exception
     }
 }
